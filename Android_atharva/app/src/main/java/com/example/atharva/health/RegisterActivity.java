cad13b1b20ba5d566b3ba5948d9740f41b10f135
Atharva Kalsekar <kalsekar.atharva7@gmail.com>
2018-04-07 12:06:46 +0530

    added the Login and register activities

---
+++ Android_atharva/app/src/main/java/com/example/atharva/health/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.example.atharva.health;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+
+
+    public void onClickLoginLink(View view) {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
